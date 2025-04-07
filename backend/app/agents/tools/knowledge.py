11008c84ba288522fde3f60c60bafe45671e4149
badals <badal@test.com>
2025-04-07 11:41:11 +0530

    sync test

---
+++ backend/app/agents/tools/knowledge.py
@@ -0,0 +1,57 @@
+import logging
+
+from app.agents.tools.agent_tool import AgentTool
+from app.repositories.models.custom_bot import BotModel
+from app.routes.schemas.conversation import type_model_name
+from app.vector_search import search_related_docs
+
+from pydantic import BaseModel, Field
+
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+
+class KnowledgeToolInput(BaseModel):
+    query: str = Field(
+        description="Input suitable for vector search, full text search, and hybrid search. When searching continuously, the query must be designed so that it does not overlap with past contexts."
+    )
+
+
+def search_knowledge(
+    tool_input: KnowledgeToolInput, bot: BotModel | None, model: type_model_name | None
+) -> list:
+    assert bot is not None
+
+    query = tool_input.query
+    logger.info(f"Running AnswerWithKnowledgeTool with query: {query}")
+
+    try:
+        search_results = search_related_docs(
+            bot,
+            query=query,
+        )
+
+        # # For testing purpose
+        # search_results = dummy_search_results
+
+        return search_results
+
+    except Exception as e:
+        logger.error(f"Failed to run AnswerWithKnowledgeTool: {e}")
+        raise e
+
+
+def create_knowledge_tool(bot: BotModel) -> AgentTool:
+    description = (
+        "Answer a user's question using information. The description is: {}".format(
+            bot.knowledge.__str_in_claude_format__()
+        )
+    )
+    logger.info(f"Creating knowledge base tool with description: {description}")
+    return AgentTool(
+        name=f"knowledge_base_tool",
+        description=description,
+        args_schema=KnowledgeToolInput,
+        function=search_knowledge,
+    )
